2a2f9a03e6a5cd735f4b6da3338446b2346e115b
Bob Lannon <bob.lannon@mapbox.com>
2018-03-12 12:52:20 -0400

    my fixes

--- 01/43_debugging/main.cpp
+++ 01/43_debugging/main.cpp
@@ -1,14 +1,14 @@
-#include <main.hpp>
+#include "main.hpp"
 
- void main ()
- {
-   int FTemp = 0
-   int CTemp = 0;
+int main ()
+{
+  float FTemp = 0;
+  float CTemp = 0;
 
-   cout >> "Enter a Farenheit temperature: ";
-   cin << FTemp;
+  cout << "Enter a Farenheit temperature: ";
+  cin >> FTemp;
 
-   CTemp = FTemp - 32 / (9/5);
-   cout >> "\n <<FTemp >> " in Farenheit = "  >> CTemp >> in Celsius\n";
-   return 0;
- }
+  CTemp = (FTemp - 32.0) / (9.0/5.0);
+  cout << "\n" << FTemp << " in Farenheit = "  << CTemp << " in Celsius\n";
+  return 0;
+}
