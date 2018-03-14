387df77503ce4a4aca211fddeaea7bc88492d90b
Bob Lannon <bob.lannon@mapbox.com>
2018-03-14 15:52:36 -0400

    solution

--- 04/10_if-else/main.cpp
+++ 04/10_if-else/main.cpp
@@ -1,5 +1,5 @@
 /*Goal: practice if-else statements in C++
-**Write a program to select the best pet. 
+**Write a program to select the best pet.
 */
 
 #include<iostream>
@@ -10,21 +10,32 @@ int main()
    std::cout<<"Would you like an animal with fur(f), feathers(t), or scales(s)?";
    std::cin>>skin;
    std::cout<<skin<<"\n";
-   
+
    //Use if-else statements to control program flow
-   
-   std::cout<<"Get a dog"<<"\n";
-   std::cout<<"Get a bird"<<"\n";
-   
-   std::cout<<"Would you like an animal that lives in water(w), land(l), or both(b)?";
-   std::cin>>location;
-   std::cout<<location<<"\n";
-   
-   //Use if-else statements to control program flow
-   
-   std::cout<<"Get a fish"<<"\n";  
-   std::cout<<"Get a gecko"<<"\n";
-   std::cout<<"Get a frog"<<"\n";
-   
+
+   if (skin == 'f') {
+       std::cout<<"Get a dog"<<"\n";
+   } else if (skin == 't') {
+       std::cout<<"Get a bird"<<"\n";
+   } else if (skin == 's') {
+       std::cout<<"Would you like an animal that lives in water(w), land(l), or both(b)?";
+       std::cin>>location;
+       std::cout<<location<<"\n";
+
+       //Use if-else statements to control program flow
+
+       if (location == 'w') {
+           std::cout<<"Get a fish"<<"\n";
+       } else if (location == 'l') {
+           std::cout<<"Get a gecko"<<"\n";
+       } else if (location == 'b') {
+           std::cout<<"Get a frog"<<"\n";
+       } else {
+           std::cout << "Sorry, location must be one of: water(w), land(l), or both(b).\n";
+       }
+   } else {
+       std::cout << "Sorry, skin must be one of: fur(f), feathers(t), or scales(s).\n";
+   }
+
    return 0;
 }
