3fcae63a288ef1255342ee48153bf3e6b6fcd21e
Bob Lannon <bob.lannon@mapbox.com>
2018-03-14 14:07:31 -0400

    fixed

--- 03/09_correct-variable-type/main.cpp
+++ 03/09_correct-variable-type/main.cpp
@@ -1,16 +1,16 @@
-/*Goal: fix the variable types problem. 
+/*Goal: fix the variable types problem.
 **This program outputs the wrong answer
-**even though it compiles and executes without errors. 
+**even though it compiles and executes without errors.
 **Fix it so that it outputs the correct value.
 */
 
 #include <iostream>
 int main(void)
 {
-    int numerator = 4;
-    int denominator = 5;
+    float numerator = 4.0;
+    float denominator = 5.0;
 
-    int answer = numerator / denominator;
+    float answer = numerator / denominator;
     std::cout<<"answer = "<<answer;
     return 0;
 }
