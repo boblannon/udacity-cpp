216798c11d94fddd6e71ab9b8fae8db8d2abef53
Bob Lannon <bob.lannon@mapbox.com>
2018-03-27 23:10:53 -0400

    apparently cin.ignore was required

--- 05/05_pointers/main.cpp
+++ 05/05_pointers/main.cpp
@@ -4,15 +4,40 @@
 */
 #include<iostream>
 #include<string>
+#include<sstream>
 
 int main()
 {
     int givenInt;
     float givenFloat;
-    double givenDouble ;
+    double givenDouble;
     std::string givenString;
     char givenChar;
 
+    std::cin >> givenInt;
+    std::cin >> givenFloat;
+    std::cin >> givenDouble;
+    std::cin.ignore();
+    std::cin >> givenChar;
+    std::cin.ignore();
+    std::getline(std::cin, givenString);
+
+    char * pointerToGivenChar = &givenChar;
+
+    std::cout << "givenInt is " << givenInt << "\n";
+    std::cout << "givenInt is stored at " << &givenInt << "\n";
+
+    std::cout << "givenFloat is " << givenFloat << "\n";
+    std::cout << "givenFloat is stored at " << &givenFloat << "\n";
+
+    std::cout << "givenDouble is " << givenDouble << "\n";
+    std::cout << "givenDouble is stored at " << &givenDouble << "\n";
+
+    std::cout << "givenString is " << givenString << "\n";
+    std::cout << "givenString is stored at " << &givenString << "\n";
+
+    std::cout << "givenChar is " << givenChar << "\n";
+    std::cout << "givenChar is stored at " << &pointerToGivenChar << "\n";
 
     return 0;
 }
